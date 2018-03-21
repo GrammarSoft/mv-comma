ebfe1f4ab311f3e689f62547553ceb26957bfd13
Tino Didriksen <mail@tinodidriksen.com>
2018-03-21 09:23:22 +0000

    Kommaforslag 2018-03-21 09:23:00

--- debug/index.php
+++ debug/index.php
@@ -2,7 +2,7 @@
 
 header('Content-Type: text/plain; charset=UTF-8');
 
-echo "Kommaforslag at commit 55a9c53c77204dc81c5f83ca02179ccdd4afbd02 (r169) deployed on 2018-02-26 13:26:54\n\n";
+echo "Kommaforslag at commit 57413239d64f569233e358d36b302132eb1a692e (r170) deployed on 2018-03-21 09:23:00\n\n";
 
 echo "uptime:\n", shell_exec('uptime'), "\n";
 echo "free -h:\n", shell_exec('free -h'), "\n";
